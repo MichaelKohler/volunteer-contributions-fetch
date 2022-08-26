70010f54f22b40a0ac67f6bf5da39068c7e99d25
Michael Kohler <me@michaelkohler.info>
2022-08-26 22:46:36 +0200

    fix(wiki): respect enabled flag or presence of config for mediaWiki

---
+++ fixtures/wiki.js
@@ -0,0 +1,57 @@
+const firstMonth = `
+  <rss version="2.0">
+    <channel>
+      <title>MozillaWiki - User contributions [en]</title>
+      <link>https://wiki.mozilla.org/Special:Contributions/User</link>
+      <description>User contributions</description>
+      <language>en</language>
+      <generator>MediaWiki 1.27.4</generator>
+      <lastBuildDate>Fri, 26 Aug 2022 20:13:05 GMT</lastBuildDate>
+      <item>
+        <title>PageA</title>
+        <link>LinkA</link>
+        <guid isPermaLink="false">GUID</guid>
+        <description>Blabla</description>
+        <pubDate>Sat, 01 Jan 2022 00:00:00 GMT</pubDate>
+        <dc:creator>User</dc:creator>
+        <comments>https://wiki.mozilla.org/Talk:PageA</comments>
+      </item>
+      <item>
+        <title>PageB</title>
+        <link>LinkB</link>
+        <guid isPermaLink="false">GUID</guid>
+        <description>Blabla</description>
+        <pubDate>Sun, 02 Jan 2022 00:00:00 GMT</pubDate>
+        <dc:creator>User</dc:creator>
+        <comments>https://wiki.mozilla.org/Talk:PageA</comments>
+      </item>
+    </channel>
+  </rss>
+`;
+
+const secondMonth = `
+  <rss version="2.0">
+    <channel>
+      <title>MozillaWiki - User contributions [en]</title>
+      <link>https://wiki.mozilla.org/Special:Contributions/User</link>
+      <description>User contributions</description>
+      <language>en</language>
+      <generator>MediaWiki 1.27.4</generator>
+      <lastBuildDate>Fri, 26 Aug 2022 20:13:05 GMT</lastBuildDate>
+      <item>
+        <title>PageC</title>
+        <link>LinkC</link>
+        <guid isPermaLink="false">GUID</guid>
+        <description>Blabla</description>
+        <pubDate>Wed, 01 Dec 2021 00:00:00 GMT</pubDate>
+        <dc:creator>User</dc:creator>
+        <comments>https://wiki.mozilla.org/Talk:PageC</comments>
+      </item>
+    </channel>
+  </rss>
+`;
+
+module.exports = {
+  firstMonth,
+  secondMonth,
+};
